4526205b7ffa56bb54f004d1c0efd36c17ea406d
Guilherme Holz <guilherme.holz@mailtop.com.br>
2019-03-29 18:00:23 -0300

    update example

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Timeline from '../../src/Timeline'
+import TimelineEvent from '../../src/TimelineEvent'
 
 const App = props => {
-  return <div />
+  return (
+    <Timeline>
+      <TimelineEvent />
+      <TimelineEvent color='#87a2c7' />
+    </Timeline>
+  )
 }
 
 export default App
