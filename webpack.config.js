73039f112b9de87bc419f4ac8b31c594f2c1a865
Guilherme Holz <guilherme.holz@mailtop.com.br>
2019-04-01 17:57:25 -0300

    chore: add file-loader to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,42 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, 'examples/src/index.html'),
-    filename: './index.html'
+  template: path.join(__dirname, 'examples/src/index.html'),
+  filename: './index.html'
 })
 
 module.exports = {
-    entry: path.join(__dirname, 'examples/src/index.js'),
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+  entry: path.join(__dirname, 'examples/src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
             }
+          }
         ]
-    },
-    plugins: [htmlWebpackPlugin],
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devServer: {
-        port: 3001
-    }
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    port: 3001
+  }
 }
