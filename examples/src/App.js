f8943f7cf037ee81e1a288ce42a13babe9cd0ef6
Guilherme Holz <guilherme.holz@mailtop.com.br>
2019-04-01 17:58:41 -0300

    feat: update example

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,12 +1,53 @@
 import React from 'react'
+import {
+  FaBug,
+  FaCogs,
+  FaPaperPlane,
+  FaRegCalendarCheck,
+  FaRegCheckCircle,
+  FaRegFileAlt
+} from 'react-icons/fa'
 import Timeline from '../../src/Timeline'
 import TimelineEvent from '../../src/TimelineEvent'
 
 const App = props => {
   return (
-    <Timeline>
-      <TimelineEvent />
-      <TimelineEvent color='#87a2c7' />
+    <Timeline minEvents={7} placeholder>
+      <TimelineEvent
+        icon={FaRegFileAlt}
+        title='Em rascunho'
+        subtitle='26/03/2019 09:51'
+      />
+      <TimelineEvent
+        color='#87a2c7'
+        icon={FaRegCalendarCheck}
+        title='Agendado'
+        subtitle='26/03/2019 09:51'
+      />
+      <TimelineEvent
+        color='#ffcc00'
+        icon={FaCogs}
+        title='Processando'
+        subtitle='26/03/2019 09:51'
+      />
+      <TimelineEvent
+        color='#9c2919'
+        icon={FaBug}
+        title='Processando'
+        subtitle='26/03/2019 09:51'
+      />
+      <TimelineEvent
+        color='#3865a3'
+        icon={FaPaperPlane}
+        title='Enviando'
+        subtitle='26/03/2019 09:51'
+      />
+      <TimelineEvent
+        color='#64a338'
+        icon={FaRegCheckCircle}
+        title='Concluído'
+        subtitle='26/03/2019 09:51'
+      />
     </Timeline>
   )
 }
