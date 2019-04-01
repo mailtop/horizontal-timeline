c7e8f15041657b84c6955fbbfd90b70e81650d4a
Guilherme Holz <guilherme.holz@mailtop.com.br>
2019-04-01 17:59:28 -0300

    feat: implement the Timeline and TimelineEvent components

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import classNames from 'classnames'
+import Grid from '@material-ui/core/Grid'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import withStyles from '@material-ui/core/styles/withStyles'
+import 'typeface-roboto'
+import Placeholder from './Placeholder'
 
-const Timeline = ({ children }) => {
-  return <div>{children}</div>
+const styles = {
+  wrapper: {
+    overflowX: 'auto'
+  },
+  container: {
+    display: 'inline-flex',
+    flex: '1 1 auto'
+  },
+  middle: {
+    marginLeft: -10
+  }
 }
 
-export default Timeline
+const renderPlaceholders = quantity => {
+  if (quantity) {
+    const placeholders = []
+    let index = 0
+
+    while (index < quantity) {
+      placeholders.push(<Placeholder />)
+      index++
+    }
+
+    return placeholders
+  }
+}
+
+const Timeline = ({ classes, children, minEvents = 0, placeholder }) => {
+  const difference = minEvents - children.length
+  const placeholders = renderPlaceholders(difference)
+
+  return (
+    <div className={classes.wrapper}>
+      <CssBaseline />
+      <Grid className={classes.container}>
+        {children.map((child, index) => {
+          return (
+            <Grid
+              key={index}
+              className={classNames({
+                [classes.middle]: index > 0 && index < children.length
+              })}
+            >
+              {child}
+            </Grid>
+          )
+        })}
+        {minEvents > 0 && difference > 0 && placeholder && (
+          <Fragment>
+            {placeholders.map((placeholder: Placeholder, index) => (
+              <Grid key={index} className={classes.middle}>
+                <Placeholder />
+              </Grid>
+            ))}
+          </Fragment>
+        )}
+      </Grid>
+    </div>
+  )
+}
+
+export default withStyles(styles)(Timeline)
--- src/TimelineEvent.jsx
+++ src/TimelineEvent.jsx
@@ -1,15 +1,93 @@
 import React from 'react'
-import { FaRegFileAlt } from 'react-icons/fa'
-import Arrow from './Arrow'
-import Circle from './Circle'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import withStyles from '@material-ui/core/styles/withStyles'
 
-const TimelineEvent = ({ color = '#e0e0e0' }) => {
+const styles = {
+  container: {
+    position: 'relative'
+  },
+  icon: {
+    position: 'absolute',
+    top: 16,
+    left: 92.5,
+    fontSize: 40,
+    color: '#fff'
+  },
+  textContainer: {
+    marginTop: -40,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    marginTop: 16,
+    textAlign: 'center'
+  },
+  button: {
+    color: '#fff'
+  }
+}
+
+const TimelineEvent = ({
+  classes,
+  color = '#e0e0e0',
+  icon: Icon,
+  title,
+  subtitle,
+  action
+}) => {
   return (
-    <div>
-      <Arrow color={color} />
-      <Circle icon={FaRegFileAlt} color={color} />
+    <div className={classes.container}>
+      {Icon && <Icon className={classes.icon} />}
+      <svg width={225} height={187} viewBox='0 0 59.531 49.477'>
+        <g transform='matrix(.99959 0 0 .99838 -100.96 -38.57)'>
+          <path
+            d='M101.002 69.656h55.492l4.064 4.158-4.064 4.205h-55.492l3.85-4.205z'
+            fill={color}
+            strokeWidth={0.24}
+          />
+          <circle
+            cx={130.726}
+            cy={73.838}
+            r={1.522}
+            fill='#fff'
+            strokeWidth={0.15}
+          />
+          <circle
+            cx={130.78}
+            cy={48.202}
+            r={9.57}
+            fill={color}
+            strokeWidth={0.194}
+          />
+          <rect
+            width={0.794}
+            height={14.363}
+            x={130.383}
+            y={56.309}
+            ry={0}
+            fill={color}
+            strokeWidth={0.108}
+          />
+        </g>
+      </svg>
+      <div className={classes.textContainer}>
+        <Typography variant='h6'>{title}</Typography>
+        <Typography variant='caption'>{subtitle}</Typography>
+      </div>
+      {action && (
+        <div className={classes.buttonContainer}>
+          <Button
+            size='small'
+            onClick={action.onClick}
+            className={classes.button}
+            style={{ backgroundColor: color }}
+          >
+            {action.label}
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
 
-export default TimelineEvent
+export default withStyles(styles)(TimelineEvent)
