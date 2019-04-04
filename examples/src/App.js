cc6b398064037a02d235525a39aeae839289e5b9
Guilherme Holz <guilherme.holz@mailtop.com.br>
2019-04-04 16:01:25 -0300

    refactor(example): add a container to the example

--- examples/src/App.js
+++ examples/src/App.js
@@ -7,53 +7,65 @@ import {
   FaRegCheckCircle,
   FaRegFileAlt
 } from 'react-icons/fa'
+import Grid from '@material-ui/core/Grid'
+import withStyles from '@material-ui/core/styles/withStyles'
 import Timeline from '../../src/Timeline'
 import TimelineEvent from '../../src/TimelineEvent'
 
-const App = props => {
+const styles = {
+  container: {
+    marginTop: 16
+  }
+}
+
+const App = ({ classes }) => {
   return (
-    <Timeline minEvents={7} placeholder>
-      <TimelineEvent
-        icon={FaRegFileAlt}
-        title='Em rascunho'
-        subtitle='26/03/2019 09:51'
-      />
-      <TimelineEvent
-        color='#87a2c7'
-        icon={FaRegCalendarCheck}
-        title='Agendado'
-        subtitle='26/03/2019 09:51'
-        action={{
-          label: 'Ver detalhes...',
-          onClick: () => console.log('test')
-        }}
-      />
-      <TimelineEvent
-        color='#ffcc00'
-        icon={FaCogs}
-        title='Processando'
-        subtitle='26/03/2019 09:51'
-      />
-      <TimelineEvent
-        color='#9c2919'
-        icon={FaBug}
-        title='Erro'
-        subtitle='26/03/2019 09:51'
-      />
-      <TimelineEvent
-        color='#3865a3'
-        icon={FaPaperPlane}
-        title='Enviando'
-        subtitle='26/03/2019 09:51'
-      />
-      <TimelineEvent
-        color='#64a338'
-        icon={FaRegCheckCircle}
-        title='Concluído'
-        subtitle='26/03/2019 09:51'
-      />
-    </Timeline>
+    <Grid container alignItems='center' className={classes.container}>
+      <Grid item xs={12}>
+        <Timeline minEvents={7} placeholder>
+          <TimelineEvent
+            icon={FaRegFileAlt}
+            title='Em rascunho'
+            subtitle='26/03/2019 09:51'
+          />
+          <TimelineEvent
+            color='#87a2c7'
+            icon={FaRegCalendarCheck}
+            title='Agendado'
+            subtitle='26/03/2019 09:51'
+            action={{
+              label: 'Ver detalhes...',
+              onClick: () => console.log('test')
+            }}
+          />
+          <TimelineEvent
+            color='#ffcc00'
+            icon={FaCogs}
+            title='Processando'
+            subtitle='26/03/2019 09:51'
+          />
+          <TimelineEvent
+            color='#9c2919'
+            icon={FaBug}
+            title='Erro'
+            subtitle='26/03/2019 09:51'
+          />
+          <TimelineEvent
+            color='#3865a3'
+            icon={FaPaperPlane}
+            title='Enviando'
+            subtitle='26/03/2019 09:51'
+          />
+          <TimelineEvent
+            color='#64a338'
+            icon={FaRegCheckCircle}
+            title='Concluído'
+            subtitle='26/03/2019 09:51'
+          />
+        </Timeline>
+      </Grid>
+    </Grid>
   )
 }
 
-export default App
+export default withStyles(styles)(App)
